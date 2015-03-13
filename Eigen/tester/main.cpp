6632e93090cee6ffbf4609e2db1359f984acea1a
pmarecki <pmarecki@gmail.com>
2015-03-13 11:31:58 +0100

    Update main.cpp

--- Eigen/tester/main.cpp
+++ Eigen/tester/main.cpp
@@ -1,1 +1,11 @@
+#include "macros.h"
+#include "EigenInterface.h"
 
+int main() {
+  EigenInterface ef;
+//  ef.eigenVectors({0,1,1,0});
+//  cout << "So let us now work on implementation of other important features...\n";
+//  ef.diagonalize5x5();
+  ef.testDecompositionMatrices();
+
+}
